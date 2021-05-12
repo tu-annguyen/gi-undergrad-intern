9a8c32d929861279fa349db74793e0ca0457dc17
Tu-An Nguyen <tunhnguy@ucsc.edu>
2021-05-12 08:28:08 -0700

    Add comments

--- convert_tasks.py
+++ convert_tasks.py
@@ -5,8 +5,6 @@ def main(argv):
     infiles = []
     outfiles = []
 
-    print(argv)
-
     # Get command-line arguments for input files. Store in opts, args.
     try:
         opts, args = getopt.gnu_getopt(argv, 'f', ['files='])
@@ -28,17 +26,20 @@ def main(argv):
 
         outfile_num += 1
 
-    print(infiles)
-    print(outfiles)
-
+    # Iterate through infiles and outfiles simultaneously.
     for ifile, ofile in zip(infiles, outfiles):
         infile = open(ifile, 'r')
         outfile = open(ofile, 'w')
        
-        dict = json.load(infile)
+        dict = json.load(infile) # Parse json into a dictionary.
 
+        # Iterate through the dictionary.
         for key in dict:
             for k in dict[key]:
+                # val: Integer representation of input value. Stored in a string.
+                # unit: Unit of bytes e.g. K, M, G. Stored in a string.
+                # bi: Indicates whether or not the input value is a bibyte or not with the presence of 'i'.
+                # byte: Stores the letter 'B.' Confirms the input value is a unit of bytes.
                 val, unit, bi, byte = 0, None, None, None 
                 
                 try:
@@ -46,11 +47,12 @@ def main(argv):
                 except (AttributeError, TypeError):
                     pass
 
+                # Convert the unit into bytes and overwrite the conversion into dict.
                 if byte is not None:
                     val_converted = convert(val, unit, bi)
                     dict[key][k] = val_converted
 
-        outfile.write(json.dumps(dict, indent = 4))
+        outfile.write(json.dumps(dict, indent = 4)) # Write translated json dict into outfile.
         
         infile.close()
         outfile.close()
